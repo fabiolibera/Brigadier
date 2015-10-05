a4df6f7f3bfed8a4bab528998b7d0a61b1542f5f
fabiolibera@gmail.com <fabiolibera@gmail.com>
2015-10-05 16:07:27 -0300

    criacao classe produto

---
+++ Brigadier.LojaVirtual.Dominio/Entidades/Produto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Brigadier.LojaVirtual.Dominio.Entidades
+{
+    public class Produto
+    {
+        public int     ProdutoId    { get; set; }
+        public string  ProdutoName  { get; set; }
+        public string  ProdutoDescr { get; set; }
+        public decimal ProdutoPreco { get; set; }
+        public string  ProdutoCateg { get; set; }
+    }
+}
