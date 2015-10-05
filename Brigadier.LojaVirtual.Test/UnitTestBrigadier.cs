fdd65400768e6d1202bbb4921f28446e5edafe28
fabiolibera@gmail.com <fabiolibera@gmail.com>
2015-10-05 15:45:40 -0300

    Teste1

---
+++ Brigadier.LojaVirtual.Test/UnitTestBrigadier.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Brigadier.LojaVirtual.Test
+{
+    [TestClass]
+    public class UnitTestBrigadier
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
